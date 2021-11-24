7f3d852c53637b9546fb36fc3cbb33e957c61504
Lyz03 <lizoe@netcourrier.com>
2021-11-24 16:43:01 +0100

    start 79

--- exo.js
+++ exo.js
@@ -1,3 +1,26 @@
 let pomme = "Elle est belle ma pomme !";
 
 console.log(pomme.lastIndexOf('m'));
+
+// exo 1
+document.getElementById('index-pomme').innerText = pomme.indexOf('pomme').toString();
+
+// exo 2
+document.getElementById('last-index-m').innerText = pomme.lastIndexOf('m').toString();
+
+// exo 3
+if (pomme.startsWith('Elle')) {
+    console.log(pomme + ' Commence par Elle');
+}
+
+// exo 4
+if (pomme.endsWith('!')) {
+    console.log(pomme + ' Se termine par un point d\'exclamation');
+}
+
+// exo 5
+document.getElementById('pomme').innerText = pomme.substring(18, 23);
+
+// exo 6
+let array = pomme.split(' ');
+console.log(array);
