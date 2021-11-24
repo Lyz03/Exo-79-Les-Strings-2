de476e7102bb1222ed721a217b729dffecc4c881
Lyz03 <lizoe@netcourrier.com>
2021-11-24 17:32:24 +0100

    exo 79

--- exo.js
+++ exo.js
@@ -9,18 +9,19 @@ document.getElementById('index-pomme').innerText = pomme.indexOf('pomme').toStri
 document.getElementById('last-index-m').innerText = pomme.lastIndexOf('m').toString();
 
 // exo 3
-if (pomme.startsWith('Elle')) {
+if (pomme.startsWith('Elle'))
     console.log(pomme + ' Commence par Elle');
-}
 
 // exo 4
-if (pomme.endsWith('!')) {
+if (pomme.endsWith('!'))
     console.log(pomme + ' Se termine par un point d\'exclamation');
-}
 
 // exo 5
 document.getElementById('pomme').innerText = pomme.substring(18, 23);
 
 // exo 6
-let array = pomme.split(' ');
-console.log(array);
+let array = pomme.split('');
+let oneLine = document.getElementById('one-line');
+
+for (let i = 0; i < array.length; i++)
+    oneLine.innerText += array[i] + '\n';
